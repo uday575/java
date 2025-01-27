317a33c74c66e15ed595040235f54eb5aa756aad
uday575 <dandu3896@gmail.com>
2025-01-27 15:38:11 +0530

    Create StringAddition.java

---
+++ StringAddition.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String s2=sc.nextLine();
+        int res=Integer.parseInt(s)+Integer.parseInt(s2);
+        System.out.println(res);
+    }
+}
